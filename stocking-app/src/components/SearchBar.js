c4ad93f46ffb0854d7eaefda5825c76cd535108c
josephjdkim <josephjdkim@gmail.com>
2020-04-27 22:05:40 -0400

    changed how secrets are handled

--- stocking-app/src/components/SearchBar.js
+++ stocking-app/src/components/SearchBar.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 
 function SearchBar() {
+  const URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=';
+  var symbol = 'AAPL';
+  
+  // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
   return (
-    // https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo
     <h1>SearchBar</h1>
   );
 }
