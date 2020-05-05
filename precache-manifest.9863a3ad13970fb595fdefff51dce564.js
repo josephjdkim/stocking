a47b661e966df8d882932288db5675244efaf2ca
josephjdkim <josephjdkim@gmail.com>
2020-05-05 15:45:12 -0400

    Updates

---
+++ precache-manifest.9863a3ad13970fb595fdefff51dce564.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2233be19300b0b0762d4cb92e1f9c304",
+    "url": "/IMA-FE-jquery/index.html"
+  },
+  {
+    "revision": "b30097b757484b1d5968",
+    "url": "/IMA-FE-jquery/static/css/main.fcf64039.chunk.css"
+  },
+  {
+    "revision": "92be9859a98777d96734",
+    "url": "/IMA-FE-jquery/static/js/2.a954b53f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IMA-FE-jquery/static/js/2.a954b53f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b30097b757484b1d5968",
+    "url": "/IMA-FE-jquery/static/js/main.b0e97dab.chunk.js"
+  },
+  {
+    "revision": "04bbb9a20ff79b67dae7",
+    "url": "/IMA-FE-jquery/static/js/runtime-main.7932d794.js"
+  }
+]);
