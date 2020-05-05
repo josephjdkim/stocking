d375c60ee6f70f26441665ed88d3080a7007bab4
josephjdkim <josephjdkim@gmail.com>
2020-05-05 04:40:55 -0400

    clicking StockCard updates stockData context

--- stocking-app/src/components/StockCard.js
+++ stocking-app/src/components/StockCard.js
@@ -1,9 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
+import { StockDataContext } from '../contexts/StockDataContext'
 
 function StockCard(props) {
+  const { setStockData } = useContext(StockDataContext);
   const className = "stockCard " + props.theme + "-stockCard";
+  
+  function updateStockData() {
+    setStockData({
+      name: props.stock.name,
+      symbol: props.stock.symbol,
+      open:  props.stock.open,
+      high: props.stock.high,
+      low:  props.stock.low,
+      close: props.stock.close,
+      change: props.stock.change,
+      volume: props.stock.volume
+    });
+  };
+
   return (
-    <div className={className}>
+    <div className={className} onClick={updateStockData}>
       <h3>{props.stock.name}</h3>
       <h3>{props.stock.symbol}</h3>
       <h3>{props.stock.change}</h3>
